11adfd39402ca04eb4283fe5003eba6efee34640
thomasvloo <vanloo.thomas@yahoo.com>
2025-01-07 10:48:23 +0100

    complete about me text

--- src/views/AboutMeView/AboutMeView.tsx
+++ src/views/AboutMeView/AboutMeView.tsx
@@ -29,6 +29,17 @@ function AboutMe() {
           The answer to those questions then seemed clear to me: My attention
           thus far was most engaged when I was contributing as a full stack
           engineer to a web application.
+          <br />
+          As for myself, I was born and raised in southern California to a
+          French mother and Dutch father. When I was 10 we moved to Munich,
+          Germany, where I have remained since. I love traveling around the
+          world, especially warm places near the ocean, I love diving and
+          snorkeling and recently started surfing. I read fiction and
+          non-fiction every day (most recently I've been absolutely captivated
+          by the sc-fi series "Red Rising" by Pierce Brown). I've been training
+          calisthenics and with weights for the better part of a decade, and
+          more recently have added bouldering and kick boxing to the mix. I also
+          have a husky, Mia who is 13 years old and has my whole heart.
         </p>
       </div>
     </div>
