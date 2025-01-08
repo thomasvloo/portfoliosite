76ba3fe6facb5747d32b9a390a2b4a8fd675ae58
thomasvloo <vanloo.thomas@yahoo.com>
2025-01-08 11:15:49 +0100

    add education item type

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface EducationDetails {
+  schoolName: string;
+  startYear: number;
+  endYear: number;
+  degree?: string;
+  subject?: string;
+  thesis?: string;
+  grade?: number;
+  location: string;
+}
