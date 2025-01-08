c2118126ebf6a4ed5a8bb0bf885f2aa10be8dd04
thomasvloo <vanloo.thomas@yahoo.com>
2025-01-08 16:14:30 +0100

    turn workitems into accordions

--- src/components/workItem/workItem.tsx
+++ src/components/workItem/workItem.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./workItem.css";
 import { WorkDetails } from "types/types";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 interface WorkItemProps {
   work: WorkDetails;
@@ -10,26 +14,57 @@ function WorkItem({ work }: WorkItemProps) {
   const { company, position, startYear, endYear, location, description } = work;
   return (
     <div className="workItem">
-      <p className="years">
-        {startYear} - {endYear}
-      </p>
-      <div className="workDetails">
-        <p className="company">{company}</p>
-
-        <p className="labelValueRow">
-          <span className="label">Position:</span>
-          <span className="value">{position}</span>
-        </p>
-        <p className="labelValueRow">
-          <span className="label">Location:</span>
-          <span className="value">{location}</span>
-        </p>
-        <p className="labelValueRow">
-          <span className="label">Description:</span>
-          <span className="value">{description}</span>
-        </p>
-      </div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="work-content"
+          id="work-header"
+        >
+          <div className="accordionSummaryContent">
+            <span className="years">
+              {startYear} - {endYear}
+            </span>
+            <span className="company">{company}</span>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div className="workDetails">
+            <p className="labelValueRow">
+              <span className="label">Position:</span>
+              <span className="value">{position}</span>
+            </p>
+            <p className="labelValueRow">
+              <span className="label">Location:</span>
+              <span className="value">{location}</span>
+            </p>
+            <p className="labelValueRow">
+              <span className="label">Description:</span>
+              <span className="value">{description}</span>
+            </p>
+          </div>
+        </AccordionDetails>
+      </Accordion>
     </div>
+    // <div className="workItem">
+    //     <p className="years">
+    //       {startYear} - {endYear}
+    //     </p>
+    //     <div className="workDetails">
+    //       <p className="company">{company}</p>
+    //       <p className="labelValueRow">
+    //         <span className="label">Position:</span>
+    //         <span className="value">{position}</span>
+    //       </p>
+    //       <p className="labelValueRow">
+    //         <span className="label">Location:</span>
+    //         <span className="value">{location}</span>
+    //       </p>
+    //       <p className="labelValueRow">
+    //         <span className="label">Description:</span>
+    //         <span className="value">{description}</span>
+    //       </p>
+    //     </div>
+    // </div>
   );
 }
 
--- src/views/WorkView/WorkView.tsx
+++ src/views/WorkView/WorkView.tsx
@@ -11,7 +11,48 @@ function Work() {
       startYear: "2021",
       endYear: "Present",
       location: "Munich, Germany",
-      description: "TBD",
+      description:
+        "Worked in financial services consulting, specializing in digital assets, blockchain, cryptocurrency, generative AI, and data engineering. Engaged in diverse projects, including web and backend development, technical due diligence, code auditing, policy writing, and developing generative AI applications. Supported clients in navigating technical and strategic transformations within the financial sector.",
+    },
+    {
+      id: 1,
+      company: "PwC Germany",
+      position: "Working Student",
+      startYear: "2021",
+      endYear: "2021",
+      location: "Munich, Germany",
+      description:
+        "Contributed to the financial services consulting sector, focusing on blockchain and cryptocurrency. Developed a web application for blockchain auditing purposes.",
+    },
+    {
+      id: 2,
+      company: "Opitz Consulting",
+      position: "Working Student",
+      startYear: "2019",
+      endYear: "2021",
+      location: "Munich, Germany",
+      description:
+        "Supported AI application development, leveraging various cloud platforms for computing and deployment. Contributed to technical consulting projects by assisting in the design and implementation of innovative AI-driven solutions.",
+    },
+    {
+      id: 3,
+      company: "ESRI South Africa",
+      position: "Intern",
+      startYear: "2016",
+      endYear: "2016",
+      location: "Cape Town, South Africa",
+      description:
+        "Assisted with software testing by identifying and reporting bugs across various features. Supported geo-referencing efforts by digitizing geographical data for regional projects, contributing to accurate and reliable geospatial solutions.",
+    },
+    {
+      id: 4,
+      company: "3-GIS",
+      position: "Intern",
+      startYear: "2015",
+      endYear: "2015",
+      location: "Decatur, Alabama",
+      description:
+        "Worked as part of the testing team, focusing on identifying and reporting software bugs to enhance product quality and reliability. Assisted in ensuring smooth functionality through detailed testing and documentation.",
     },
   ];
 
