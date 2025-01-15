fce9d231a849d745eccdda9e28f71c11f0ac7627
thomasvloo <vanloo.thomas@yahoo.com>
2025-01-15 16:30:53 +0100

    add certification downloadables

---
+++ src/types/certifications.ts
@@ -0,0 +1,14 @@
+export const certifications = [
+  {
+    name: 'The Complete 2024 Web Development Bootcamp',
+    file: '/assets/certifications/webdevCert.pdf',
+  },
+  {
+    name: 'Hyperledger Fabric 2.X Network Deisgn and Setup',
+    file: '/assets/certifications/fabricCert2.pdf',
+  },
+  {
+    name: 'Hyperledger Fabric 2.x - First Practical Blockchain [2022]',
+    file: '/assets/certifications/fabricCert1.pdf',
+  },
+];
--- src/views/ReferralsView/ReferralsView.tsx
+++ src/views/ReferralsView/ReferralsView.tsx
@@ -1,9 +1,31 @@
+import './ReferralsView.css';
+
 import React from 'react';
+import { certifications } from 'types/certifications';
 
 function ReferralsView() {
   return (
-    <div className="container">
-      <h1>Hi from Referrals!</h1>
+    <div className="referralCertContainer">
+      <div className="referralContainer">
+        <div className="referralHeader">
+          <h3>Referrals</h3>
+        </div>
+      </div>
+      <div className="certContainer">
+        <div className="certHeader">
+          <h3>Certifications</h3>
+        </div>
+        <ul className="certList">
+          {certifications.map((cert, index) => (
+            <li key={index} className="certItem">
+              <span className="certName">{cert.name}</span>
+              <a href={cert.file} download className="downloadLink">
+                Download
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
